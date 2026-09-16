1caca4508b4ec950ced0c995995f437cdd8ae26b
MarkDiamant <markdiamant@gmail.com>
2026-09-16 18:07:36 +0100

    Add persistent PDF action to quote page

--- app/admin/jobs/[reference]/quote/[quoteId]/page.tsx
+++ app/admin/jobs/[reference]/quote/[quoteId]/page.tsx
@@ -1,6 +1,8 @@
 import QuotePrint from "@/components/admin/QuotePrint";
+import QuotePdfActions from "@/components/admin/QuotePdfActions";
 
 export default async function QuotePage({ params }: { params: Promise<{ reference: string; quoteId: string }> }) {
   const { reference, quoteId } = await params;
-  return <QuotePrint reference={reference.toUpperCase()} quoteId={quoteId} />;
+  const ref = reference.toUpperCase();
+  return <><QuotePrint reference={ref} quoteId={quoteId} /><QuotePdfActions reference={ref} quoteId={quoteId} /></>;
 }
