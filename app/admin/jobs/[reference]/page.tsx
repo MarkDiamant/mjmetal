30b5a8f18975d6df795dec3dec68255b2d127e18
MarkDiamant <markdiamant@gmail.com>
2026-09-20 11:18:23 +0100

    Remove unconditional Xero button from job page

--- app/admin/jobs/[reference]/page.tsx
+++ app/admin/jobs/[reference]/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import JobDetail from "@/components/admin/JobDetail";
 
 export default async function JobPage({ params }: { params: Promise<{ reference: string }> }) {
@@ -6,6 +5,5 @@ export default async function JobPage({ params }: { params: Promise<{ reference:
   const ref = reference.toUpperCase();
   return <>
     <JobDetail reference={ref} />
-    <Link href={`/admin/jobs/${ref}/xero`} className="fixed bottom-5 right-5 z-40 rounded-xl bg-[#13b5ea] px-4 py-3 text-sm font-black text-white shadow-lg print:hidden">Xero invoice</Link>
   </>;
 }
