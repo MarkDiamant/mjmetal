52ff367932d496fd3e55f078f224a04707f80594
MarkDiamant <markdiamant@gmail.com>
2026-09-23 00:16:50 +0100

    Complete BMS demo detail page mirror

--- app/demo/jobs/[reference]/xero/page.tsx
+++ app/demo/jobs/[reference]/xero/page.tsx
@@ -1,1 +1,6 @@
-export {default} from "@/app/admin/jobs/[reference]/xero/page";
+import XeroInvoicePage from "@/components/demo/XeroInvoicePage";
+
+export default async function JobXeroPage({ params }: { params: Promise<{ reference: string }> }) {
+  const { reference } = await params;
+  return <XeroInvoicePage reference={reference.toUpperCase()} />;
+}
