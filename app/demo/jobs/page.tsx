7767900dfbc28b2f252ff2e88a7f99df3d4bfa3e
MarkDiamant <markdiamant@gmail.com>
2026-09-23 00:15:35 +0100

    Mirror current M&J pages into isolated BMS demo

--- app/demo/jobs/page.tsx
+++ app/demo/jobs/page.tsx
@@ -1,1 +1,2 @@
-export {default} from "@/app/admin/jobs/page";
+import BusinessDirectory from "@/components/demo/BusinessDirectory";
+export default function Page(){return <BusinessDirectory view="jobs"/>;}
