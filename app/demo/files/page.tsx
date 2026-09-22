dabae456fa6146b2f17e4af78d7718db5e9287d2
MarkDiamant <markdiamant@gmail.com>
2026-09-23 00:16:01 +0100

    Continue exact BMS demo page mirror

--- app/demo/files/page.tsx
+++ app/demo/files/page.tsx
@@ -1,1 +1,2 @@
-export {default} from "@/app/admin/files/page";
+import BusinessDirectory from "@/components/demo/BusinessDirectory";
+export default function Page(){return <BusinessDirectory view="files"/>;}
